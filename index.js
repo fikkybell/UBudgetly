b9501e5a2475a6571384533c958f018431fec42a
fikkybell <jesufikayo8@gmail.com>
2024-12-14 19:52:12 -0330

    Main page added.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+const links = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
 
 
+links.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault(); 
+
+  
+    links.forEach(link => link.classList.remove('active'));
+    this.classList.add('active');
+
+
+    const targetId = this.getAttribute('href').slice(1);
+    console.log(targetId)
+    sections.forEach(section => {
+      if (section.id === targetId) {
+        section.classList.remove('hidden');
+      } else {
+        section.classList.add('hidden');
+      }
+    });
+  });
+});
